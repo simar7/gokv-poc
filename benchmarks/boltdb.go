aeaf636f4bd7cc62328d4bcb3b32a322b1c36289
Simarpreet Singh <simar@linux.com>
2019-11-07 14:19:05 -0800

    benchmarks: Add boltdb benchmark with goroutines and results
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- benchmarks/boltdb.go
+++ benchmarks/boltdb.go
@@ -1,12 +1,124 @@
 package benchmarks
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"sync"
 
 	bolt "go.etcd.io/bbolt"
 )
+
+const NumGoRoutines = 1000
+
+func BoltBatchGoRoutines() {
+	f, err := ioutil.TempFile("", "boltdb-BoltBatchGoRoutines-*")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open the database.
+	db, err := bolt.Open(f.Name(), 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(db.Path())
+
+	var wg sync.WaitGroup
+	wg.Add(NumGoRoutines)
+
+	// Start a multiple write transaction.
+	for i := 0; i < NumGoRoutines; i++ {
+		go func(grn int) {
+			defer wg.Done()
+			if err := db.Batch(func(tx *bolt.Tx) error {
+				// Create a bucket.
+				b, err := tx.CreateBucket([]byte(fmt.Sprintf("widgets%d", grn)))
+				if err != nil {
+					return err
+				}
+
+				// Set the value "bar" for the key "foo".
+				if err := b.Put([]byte(fmt.Sprintf("foo%d", grn)), []byte("bar")); err != nil {
+					return err
+				}
+				return nil
+			}); err != nil {
+				log.Fatal(err)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+	// Read value back in a different read-only transaction.
+	//if err := db.View(func(tx *bolt.Tx) error {
+	//	value := tx.Bucket([]byte("widgets")).Get([]byte("foo"))
+	//	fmt.Printf("[batch] The value of 'foo' is: %s\n", value)
+	//	return nil
+	//}); err != nil {
+	//	log.Fatal(err)
+	//}
+
+	// Close database to release file lock.
+	if err := db.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func BoltUpdateGoRoutines() {
+	f, err := ioutil.TempFile("", "boltdb-BoltUpdateGoRoutines-*")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open the database.
+	db, err := bolt.Open(f.Name(), 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(db.Path())
+
+	var wg sync.WaitGroup
+	wg.Add(NumGoRoutines)
+
+	// Start a multiple write transaction.
+	for i := 0; i < NumGoRoutines; i++ {
+		go func(grn int) {
+			defer wg.Done()
+			if err := db.Update(func(tx *bolt.Tx) error {
+				// Create a bucket.
+				b, err := tx.CreateBucket([]byte(fmt.Sprintf("widgets%d", grn)))
+				if err != nil {
+					return err
+				}
+
+				// Set the value "bar" for the key "foo".
+				if err := b.Put([]byte(fmt.Sprintf("foo%d", grn)), []byte("bar")); err != nil {
+					return err
+				}
+				return nil
+			}); err != nil {
+				log.Fatal(err)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+	// Read value back in a different read-only transaction.
+	//if err := db.View(func(tx *bolt.Tx) error {
+	//	value := tx.Bucket([]byte("widgets")).Get([]byte("foo"))
+	//	fmt.Printf("[batch] The value of 'foo' is: %s\n", value)
+	//	return nil
+	//}); err != nil {
+	//	log.Fatal(err)
+	//}
+
+	// Close database to release file lock.
+	if err := db.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func BoltBatch() {
 	f, err := ioutil.TempFile("", "boltdb-*")
--- benchmarks/boltdb_test.go
+++ benchmarks/boltdb_test.go
@@ -3,13 +3,29 @@ package benchmarks
 import "testing"
 
 func BenchmarkBoltUpdate(b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		BoltUpdate()
 	}
 }
 
 func BenchmarkBoltBatch(b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		BoltBatch()
+	}
+}
+
+func BenchmarkBoltBatchGoRoutines(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		BoltBatchGoRoutines()
+	}
+}
+
+func BenchmarkBoltUpdateGoRoutines(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		BoltUpdateGoRoutines()
 	}
 }
